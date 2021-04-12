cfd14e403f2bf3da81f9789d418708ffb313bd92
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-04-12 23:55:54 +0200

    m02_scope: 02

--- src/m02_scope/Main.java
+++ src/m02_scope/Main.java
@@ -19,11 +19,24 @@ interface UpperLowerConcat {
 
 class AnotherClass {
     public String makeLowerCase(){
-        return Main.doStringStuff(new UpperLowerConcat() {
-            @Override
-            public String switchAndConcat(String s1, String s2) {
-                return s1.toLowerCase() + s2.toLowerCase();
-            }
-        }, "String1", "String2");
+        UpperLowerConcat ulc = (s1, s2) -> {
+            System.out.println("Lambda expression's class: " + getClass().getSimpleName());
+            String result = s1.toLowerCase() + s2.toLowerCase();
+            return result;
+        };
+        // wyrażenia lambda są traktowane jak nested blocks {...}
+        // wszystkie zmienne spoza {...} muszą być zadeklarowane jako final
+        // lub ich wartość nie może się zmieniać (czyli bez inkrementacji itp.)
+
+        System.out.println("Another class name: " + getClass().getSimpleName());
+        return Main.doStringStuff(ulc, "String1", "String2");
+//        System.out.println("Another class name: " + getClass().getSimpleName());
+//        return Main.doStringStuff(new UpperLowerConcat() {
+//            @Override
+//            public String switchAndConcat(String s1, String s2) {
+//                //System.out.println("Anonymous class name: " + getClass().getSimpleName());
+//                return s1.toLowerCase() + s2.toLowerCase();
+//            }
+//        }, "String1", "String2");
     }
 }
