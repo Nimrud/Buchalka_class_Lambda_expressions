6163a2b8ceda547d0026f1dad3379ff457af04a7
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-04-21 23:57:40 +0200

    m07_challenge3 (1)

---
+++ src/m07_challenge3/Main.java
@@ -0,0 +1,26 @@
+package m07_challenge3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // CHALLENGE 3
+        // write the code to print the list in sorted order
+        // and with the first letter in each name upper-cased ('harry' should be printed as 'Harry')
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+
+    }
+}
